ace40ad667ab555d0c811f7f3fbc1d499d7bb1c7
Cody Brunner <rockchalkwushock@icloud.com>
2016-09-25 17:29:15 -0500

    V1.1: Added server.js, modified package.json, added initial required code to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+/*
+  This script runs the server by using Express.js
+  It GETS & PUTS data to & from Client.
+*/
+
+// Represents the Express.js node module.
+var express = require('express');
+// Represents the calling of the Express module contents.
+var app = express();
+// Represents the port server will run on.
+var port = 3000;
